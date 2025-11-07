cf93238d637540d352fcd5ea5a1ee275c1ead06f
brubakerjeff <brubaker.jeff@gmail.com>
2025-11-07 08:26:26 -0500

    Update objdet_detect.py - ID_S3_EX1-3 START

--- student/objdet_detect.py
+++ student/objdet_detect.py
@@ -61,6 +61,31 @@ def load_configs_model(model_name='darknet', configs=None):
         ####### ID_S3_EX1-3 START #######     
         #######
         print("student task ID_S3_EX1-3")
+        configs = edict(vars(parser.parse_args()))
+        configs.pin_memory = True
+        configs.distributed = False  # For testing on 1 GPU only
+    
+        configs.input_size = (608, 608)
+        configs.hm_size = (152, 152)
+        configs.down_ratio = 4
+        configs.max_objects = 50
+    
+        configs.imagenet_pretrained = False
+        configs.head_conv = 64
+        configs.num_classes = 3
+        configs.num_center_offset = 2
+        configs.num_z = 1
+        configs.num_dim = 3
+        configs.num_direction = 2  # sin, cos
+    
+        configs.heads = {
+            'hm_cen': configs.num_classes,
+            'cen_offset': configs.num_center_offset,
+            'direction': configs.num_direction,
+            'z_coor': configs.num_z,
+            'dim': configs.num_dim
+        }
+        configs.num_input_features = 4
 
         #######
         ####### ID_S3_EX1-3 END #######     
@@ -82,7 +107,7 @@ def load_configs(model_name='fpn_resnet', configs=None):
     # init config file, if none has been passed
     if configs==None:
         configs = edict()    
-
+    
     # birds-eye view (bev) parameters
     configs.lim_x = [0, 50] # detection range in m
     configs.lim_y = [-25, 25]
